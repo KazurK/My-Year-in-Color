b1c6032c98c69d8eacea5924f4013704a6580e21
Jaden Foster <fosterj19@coventry.ac.uk>
2026-05-14 22:00:49 +0100

    removed edit entry from options so it dosent show up in terminal

--- terminalgui.py
+++ terminalgui.py
@@ -7,7 +7,7 @@ from cursesHelper import select_from_list
 def main_menu(stdscr):
     data = load_data()
 
-    options = ["View Calander", "Add Entry", "Edit Entry", "Delete Entry"]
+    options = ["View Calander", "Add Entry", "Delete Entry"]
     chosen_option = select_from_list(stdscr, "Select an option:", options)
 
     if chosen_option == "View Calander":
